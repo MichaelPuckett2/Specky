f3b4d0ae4d5a9afa3ef2b443071bb474bc83d886
MichaelPuckettII <mpuckett@baseload.com>
2020-06-03 19:55:29 -0400

    updated reflection helper

---
+++ Specky/Helpers/ReflectionHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Specky.Helpers
+{
+    public static class ReflectionHelper
+    {
+        public static Assembly GetAssembly<T>() => Assembly.GetAssembly(typeof(T));
+    }
+}
