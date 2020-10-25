a47bb039fc6ea7d4f5524e89380eb78c699397ed
Michael Pucket <michael_puckett_ii@hotmail.com>
2020-10-25 16:53:25 -0400

    reset branch, added ignore properly, and added proper parameter injection

---
+++ Specky.IntegrationTest/Program.cs
@@ -0,0 +1,85 @@
+﻿using Specky.Attributes;
+using Specky.Enums;
+using System;
+
+namespace Specky.IntegrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Manager.Container
+            //    .SetSpeck<Worker>()
+            //    .AsSingleton()
+            //    .As<IWorker>()
+            //    .As<IWorker2>();
+
+            Manager.AutoStrapper.Start();
+
+            var worker = Manager.Container.Get<IWorker>();
+            worker.DoWork();
+
+            //var workerAgain = Manager.Container.Get<IWorker>();
+            //workerAgain.DoWork();
+
+            //Console.WriteLine(worker == workerAgain);
+
+            var worker2 = Manager.Container.Get<IWorker2>();
+            worker2.DoWork2();
+
+            Console.WriteLine($"{ nameof(IWorker)} == {nameof(IWorker2)} : {worker == worker2}");
+
+            Console.WriteLine($"{ nameof(IWriter)} == {nameof(IWriter)} : {((Worker)worker).Writer == ((Worker)worker2).Writer}");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IWorker
+    {
+        void DoWork();
+    }
+
+    public interface IWorker2
+    {
+        void DoWork2();
+    }
+
+    //[Speck]
+    //[Speck(DeliveryMode.Singleton, new[] { typeof(IWorker), typeof(IWorker2) })]
+    [Speck(DeliveryMode.Singleton, new[] { typeof(IWorker) })]
+    [Speck(DeliveryMode.Singleton, new[] { typeof(IWorker2) })]
+    public class Worker : IWorker, IWorker2
+    {
+        public Worker(IWriter writer)
+        {
+            Writer = writer;
+        }
+
+        public IWriter Writer { get; }
+
+        public void DoWork()
+        {
+            Writer.Write($"{nameof(Worker)} is working.");
+        }
+
+        public void DoWork2()
+        {
+            Writer.Write($"{nameof(Worker)} is working on 2.");
+        }
+    }
+
+    public interface IWriter
+    {
+        public void Write(string message);
+    }
+
+    [Speck(DeliveryMode.Transient)]
+    public class ConsoleWriter : IWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Specky/Attributes/SpeckAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Specky.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Specky.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class SpeckAttribute : Attribute
+    {
+        public SpeckAttribute(DeliveryMode deliveryMode = default, Type[] interfaces = null)
+        {
+            DeliveryMode = deliveryMode;
+            Interfaces = interfaces;
+        }
+
+        public DeliveryMode DeliveryMode { get; }
+        public IEnumerable<Type> Interfaces { get; }
+    }
+}
